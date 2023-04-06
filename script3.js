8b61f5382667b97451b8d44ddb3f6a3389b3762c
FridayStark <yimeng.elsa.liu@outlook.com>
2023-04-06 04:26:46 -0400

    test and minor modification

--- script3.js
+++ script3.js
@@ -1,5 +1,8 @@
 function genCheck(x){
-	if(x<=1981 && x>=1965){
+	if(x<1965){
+		alert("Please enter any year number in 1965-2025.")
+	}
+	else if(x<=1981){
 		alert("Generation X");
 	}else if(x<=1996){
 		alert("Generation Y");
@@ -12,4 +15,9 @@ function genCheck(x){
 	}
 }
 
+genCheck(1921);
 genCheck(1970);
+genCheck(1990);
+genCheck(2002);
+genCheck(2023);
+genCheck(2077);
